07a80e61f1db912c8207aadc74cec0a157f3c8a8
Eron Castro <eroncastro@gmail.com>
2019-01-10 17:55:48 -0200

    Adds first version of garage spot search

--- src/App.js
+++ src/App.js
@@ -37,6 +37,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import parkingSpots from './ParkingSpots';
+import SearchBar from './SearchBar';
 
 const styles = theme => ({
   root: {
@@ -79,39 +80,73 @@ const partner = partnerRegister => {
     : '-';
 };
 
-function SimpleTable(props) {
-  const { classes } = props;
+const filteredApartments = (elements, term) => {
+  if (!term.trim()) return elements;
 
-  return (
-    <Paper className={classes.root}>
-      <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <TableCell>Apartmento</TableCell>
-            <TableCell align="right">Garagem</TableCell>
-            <TableCell align="right">Tipo</TableCell>
-            <TableCell align="right">Andar</TableCell>
-            <TableCell align="right">Dupla</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {parkingSpots.map((elem, id) => {
-            return (
-              <TableRow key={id}>
-                <TableCell component="th" scope="row">
-                  {`${elem.apartment}${elem.building.toUpperCase()}`}
-                </TableCell>
-                <TableCell align="right">{spot(elem.garage.spot)}</TableCell>
-                <TableCell align="right">{spotType[elem.garage.type]}</TableCell>
-                <TableCell align="right">{elem.garage.floor}</TableCell>
-                <TableCell align="right">{partner(elem.partner)}</TableCell>
+  return elements.filter(({ apartment, building }) => {
+    const [apartmentTerm, buildingTerm] = term.split(/([a-zA-Z]+)/);
+
+    try {
+      if (apartmentTerm && buildingTerm) {
+        return apartment.toString() === apartmentTerm && building === buildingTerm.toLowerCase();
+      }
+
+      return apartment.toString() === apartmentTerm || building === buildingTerm.toLowerCase();
+    } catch(e) {
+      return false;
+    }
+  });
+};
+
+class SimpleTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {searchTerm: ''};
+    this.classes = props.classes;
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ searchTerm: e.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar
+          onSearchTermChange={this.handleChange}/>
+        <Paper className={this.classes.root}>
+          <Table className={this.classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>Apartmento</TableCell>
+                <TableCell align="right">Garagem</TableCell>
+                <TableCell align="right">Tipo</TableCell>
+                <TableCell align="right">Andar</TableCell>
+                <TableCell align="right">Dupla</TableCell>
               </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table>
-    </Paper>
-  );
+            </TableHead>
+            <TableBody>
+              {filteredApartments(parkingSpots, this.state.searchTerm).map((elem, id) => {
+                return (
+                  <TableRow key={id}>
+                    <TableCell component="th" scope="row">
+                      {`${elem.apartment}${elem.building.toUpperCase()}`}
+                    </TableCell>
+                    <TableCell align="right">{spot(elem.garage.spot)}</TableCell>
+                    <TableCell align="right">{spotType[elem.garage.type]}</TableCell>
+                    <TableCell align="right">{elem.garage.floor}</TableCell>
+                    <TableCell align="right">{partner(elem.partner)}</TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </Paper>
+      </div>
+    );
+  }
 }
 
 SimpleTable.propTypes = {
