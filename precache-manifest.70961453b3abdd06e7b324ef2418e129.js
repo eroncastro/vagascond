87cb2293bcb04bf12642924587ff12ab3085b87d
Eron Castro <eroncastro@gmail.com>
2019-01-15 11:43:39 -0200

    Updates

---
+++ precache-manifest.70961453b3abdd06e7b324ef2418e129.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c260436b341e8040dd8",
+    "url": "/vagascond/static/js/runtime~main.2c260436.js"
+  },
+  {
+    "revision": "5ec61ced9ad9318b803f",
+    "url": "/vagascond/static/js/main.5ec61ced.chunk.js"
+  },
+  {
+    "revision": "783bd7c63641cba0a860",
+    "url": "/vagascond/static/js/1.783bd7c6.chunk.js"
+  },
+  {
+    "revision": "5ec61ced9ad9318b803f",
+    "url": "/vagascond/static/css/main.54189880.chunk.css"
+  },
+  {
+    "revision": "87417612c0ca552ffad1eab6cb6abe2a",
+    "url": "/vagascond/index.html"
+  }
+];
